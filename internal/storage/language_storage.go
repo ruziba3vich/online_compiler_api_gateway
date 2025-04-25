70d8192f3733abf7cde8be6665f87b4e44ef130b
ruziba3vich <rruzibayevich@gmail.com>
2025-04-26 04:06:16 +0500

    feat online_compiler: implement storage for languages

---
+++ internal/storage/language_storage.go
@@ -0,0 +1,65 @@
+package storage
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"slices"
+
+	"github.com/ruziba3vich/online_compiler_api_gateway/pkg/config"
+)
+
+type LangStorage struct {
+	filePath string
+}
+
+func NewLanguageStorage(cfg *config.Config) *LangStorage {
+	return &LangStorage{
+		filePath: cfg.LangStorageFilePath,
+	}
+}
+
+func (s *LangStorage) GetLanguages() ([]string, error) {
+	data, err := os.ReadFile(s.filePath)
+	if err != nil {
+		return []string{}, errors.New("unable to read file")
+	}
+
+	var languages []string
+	if err := json.Unmarshal(data, &languages); err != nil {
+		return []string{}, errors.New("invalid JSON format")
+	}
+
+	return languages, nil
+}
+
+func (s *LangStorage) AddLanguage(language string) error {
+	if _, err := os.Stat(s.filePath); os.IsNotExist(err) {
+		err := os.WriteFile(s.filePath, []byte("[]"), 0644)
+		if err != nil {
+			return errors.New("unable to create file")
+		}
+	}
+
+	languages, err := s.GetLanguages()
+	if err != nil {
+		return err
+	}
+
+	if slices.Contains(languages, language) {
+		return errors.New("this language already exists")
+	}
+
+	languages = append(languages, language)
+
+	newData, err := json.MarshalIndent(languages, "", "  ")
+	if err != nil {
+		return errors.New("failed to encode JSON")
+	}
+
+	if err := os.WriteFile(s.filePath, newData, 0644); err != nil {
+		return errors.New("unable to write to file")
+	}
+
+	return nil
+}
