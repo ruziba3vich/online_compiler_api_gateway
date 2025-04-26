77cde1353b351320e3a38850fb8bf1cee95243bb
ruziba3vich <rruzibayevich@gmail.com>
2025-04-26 22:55:15 +0500

    fix online_compiler: fix file storage

--- internal/storage/language_storage.go
+++ internal/storage/language_storage.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
-	"slices"
 
 	"github.com/ruziba3vich/online_compiler_api_gateway/pkg/config"
 )
@@ -22,19 +21,12 @@ func NewLanguageStorage(cfg *config.Config) *LangStorage {
 func (s *LangStorage) GetLanguages() ([]string, error) {
 	data, err := os.ReadFile(s.filePath)
 	if err != nil {
-		return []string{}, errors.New("unable to read file")
-	}
-
-	if len(data) == 0 {
-		if err := os.WriteFile(s.filePath, []byte("[]"), 0644); err != nil {
-			return []string{}, errors.New("unable to initialize empty file")
-		}
-		return []string{}, nil
+		return nil, err
 	}
 
 	var languages []string
 	if err := json.Unmarshal(data, &languages); err != nil {
-		return []string{}, errors.New("invalid JSON format")
+		return nil, err
 	}
 
 	return languages, nil
@@ -46,30 +38,24 @@ func (s *LangStorage) AddLanguage(language string) error {
 		return err
 	}
 
-	if slices.Contains(languages, language) {
-		return errors.New("this language already exists")
-	}
-
 	languages = append(languages, language)
 
-	newData, err := json.MarshalIndent(languages, "", "  ")
+	data, err := json.MarshalIndent(languages, "", "  ")
 	if err != nil {
-		return errors.New("failed to encode JSON")
+		return err
 	}
 
-	if err := os.WriteFile(s.filePath, newData, 0644); err != nil {
-		return errors.New("unable to write to file")
-	}
-
-	return nil
+	return os.WriteFile(s.filePath, data, 0644)
 }
 
 func (s *LangStorage) EnsureStorageExists() error {
-	if _, err := os.Stat(s.filePath); os.IsNotExist(err) {
-		err := os.WriteFile(s.filePath, []byte("[]"), 0644)
+	if _, err := os.Stat(s.filePath); errors.Is(err, os.ErrNotExist) {
+		emptyArray := []string{}
+		data, err := json.Marshal(emptyArray)
 		if err != nil {
-			return errors.New("unable to create file")
+			return err
 		}
+		return os.WriteFile(s.filePath, data, 0644)
 	}
 	return nil
 }
