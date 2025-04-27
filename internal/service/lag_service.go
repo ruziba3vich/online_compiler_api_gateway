f16ef2928fc7c29e9ec27f8cd2f3a06fb262316a
ruziba3vich <rruzibayevich@gmail.com>
2025-04-28 03:17:35 +0500

    fix online_compiler: implement password validation

--- internal/service/lag_service.go
+++ internal/service/lag_service.go
@@ -8,7 +8,10 @@ import (
 	"github.com/ruziba3vich/online_compiler_api_gateway/internal/dto"
 	"github.com/ruziba3vich/online_compiler_api_gateway/internal/storage"
 	logger "github.com/ruziba3vich/prodonik_lgger"
+	"golang.org/x/crypto/bcrypt"
 )
+
+var hashed string = "$2a$10$/zXjJLDbjJZcsdl0zPYIe.lcBf9rrFbjomA86CE72SX.akadbPWfi"
 
 type LangService struct {
 	langStorage *storage.LangStorage
@@ -23,6 +26,14 @@ func NewLangService(langStorage *storage.LangStorage, logger *logger.Logger) *La
 }
 
 func (s *LangService) CreateLanguage(req *dto.Language) error {
+	ok, err := compareHashedString(req.Password)
+	if err != nil {
+		s.logger.Error("failed to compare password", map[string]any{"error": err.Error()})
+		return fmt.Errorf("failed to compare password")
+	}
+	if !ok {
+		return errors.New("invalid password")
+	}
 	if len(req.Name) == 0 {
 		s.logger.Error("error while checking storage check", map[string]any{"length": len(req.Name)})
 		return errors.New("language name cannot be empty")
@@ -34,7 +45,7 @@ func (s *LangService) CreateLanguage(req *dto.Language) error {
 	if slices.Contains(languages, req.Name) {
 		return fmt.Errorf("%s already exists", req.Name)
 	}
-	return s.langStorage.AddLanguage(req)
+	return s.langStorage.AddLanguage(req.Name)
 }
 
 func (s *LangService) GetAllLanguages() ([]string, error) {
@@ -49,4 +60,15 @@ func (s *LangService) GetAllLanguages() ([]string, error) {
 		return languages, err
 	}
 	return languages, nil
+}
+
+func compareHashedString(plaintext string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(plaintext))
+	if err != nil {
+		if err == bcrypt.ErrMismatchedHashAndPassword {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
