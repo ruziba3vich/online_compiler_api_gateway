e4ec6dc5a79dff2da47264ca6f174a1b29f9a109
ruziba3vich <rruzibayevich@gmail.com>
2025-04-29 03:37:50 +0500

    fix online_compiler: use a driver that doesn't require cgo

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,13 +1,13 @@
 package db
 
 import (
+	"github.com/glebarez/sqlite"
 	"github.com/ruziba3vich/online_compiler_api_gateway/internal/models"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func NewDB(path string) (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open(path), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(path+"?_pragma=journal_mode(WAL)"), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
