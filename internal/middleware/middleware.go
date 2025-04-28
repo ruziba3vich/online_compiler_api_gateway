5d40df12381805aa57c76c0167a399e63664db4d
ruziba3vich <rruzibayevich@gmail.com>
2025-04-29 00:21:24 +0500

    feat online_compiler: implement CORS allower

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -53,3 +53,20 @@ func (m *MidWare) RateLimit() gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
+
+		c.Next()
+	}
+}
